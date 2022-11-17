efaa4d46f6091d3340886a2c51d6f613b9cc9e2a
Jordan <jordancole2245@gmail.com>
2022-11-17 14:33:16 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+'use strict'
+
+//buttons
+
+const newBook = document.querySelector('')
+const addBook = document.querySelector('')
+const closeModal = document.querySelector('')
+const deleteBook = document.querySelectorAll('')
+
+//interactable elements
+
+
+
+//library array
+
+let myLibrary = [];
+
+//constructor
+
+function Book(author, title, pages, read, genre) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+    this.genre = genre;
+}
+
+//functions
+
+function addToLibrary() {
+    
+}
+
+function displayLibrary () {
+    for(i = myLibrary.length, i > myLibrary.length, i++);
+    
+}
